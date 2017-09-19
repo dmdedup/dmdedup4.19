1c163a0aaf025df205e7873882a2327497a9e713
Nidhi Panpalia <npanpalia@cs.stonybrook.edu>
2017-09-19 10:47:45 -0400

    Removed strict patch warnings for dm-dedup-hash.h
    Signed-off-by: Nidhi Panpalia <npanpalia@cs.stonybrook.edu>

--- dm-dedup-hash.h
+++ dm-dedup-hash.h
@@ -17,7 +17,7 @@
 
 #define DEDUP_HASH_DESC_COUNT 128
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,6,0) //4.6.0-rc1
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 6, 0) /* 4.6.0-rc1 */
 #define SHASH 1
 #define crypto_alloc_hash crypto_alloc_shash
 #define crypto_hash_final crypto_shash_final
@@ -30,23 +30,26 @@
 
 static inline int crypto_has_hash(const char *alg_name, u32 type, u32 mask)
 {
-        type &= ~CRYPTO_ALG_TYPE_MASK;
-        mask &= ~CRYPTO_ALG_TYPE_MASK;
-        type |= CRYPTO_ALG_TYPE_SHASH;
-        mask |= CRYPTO_ALG_TYPE_HASH_MASK;
+	type &= ~CRYPTO_ALG_TYPE_MASK;
+	mask &= ~CRYPTO_ALG_TYPE_MASK;
+	type |= CRYPTO_ALG_TYPE_SHASH;
+	mask |= CRYPTO_ALG_TYPE_HASH_MASK;
 
-        return crypto_has_alg(alg_name, type, mask);
+	return crypto_has_alg(alg_name, type, mask);
 }
 
 struct hash_desc_table {
 #if SHASH
 	union{
-		/*Made as pointer from array to extra data allocation for context/state*/
-		struct shash_desc *shd;//[DEDUP_HASH_DESC_COUNT];
+		/*
+		 * Made as pointer from array to extra data allocation
+		 * for context/state
+		 */
+		struct shash_desc *shd; /* [DEDUP_HASH_DESC_COUNT]; */
 		/*Added void pointer for better access, to increment byte wise*/
 		void *desc;
 	};
-	uint32_t desc_state_size;
+	u32 desc_state_size;
 #else
 	struct hash_desc desc[DEDUP_HASH_DESC_COUNT];
 #endif
@@ -57,7 +60,7 @@ struct hash_desc_table {
 extern void desc_table_deinit(struct hash_desc_table *desc_table);
 extern struct hash_desc_table *desc_table_init(char *crypt_alg);
 extern int compute_hash_bio(struct hash_desc_table *desc_table,
-				struct bio *bio, char *hash);
+			    struct bio *bio, char *hash);
 extern unsigned int get_hash_digestsize(struct hash_desc_table *desc_table);
 
 #endif /* DM_DEDUP_HASH_H */
