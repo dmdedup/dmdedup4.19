8e12c6673903cdaf63445bbb01631c6e3049d401
Noopur Maheshwari <nmaheshwari@cs.stonybrook.edu>
2018-04-09 12:17:23 -0400

    Updated Copyrights

--- dm-dedup-check.c
+++ dm-dedup-check.c
@@ -1,15 +1,17 @@
 /*
- * Copyright (C) 2012-2017 Vasily Tarasov
+ * Copyright (C) 2012-2018 Vasily Tarasov
  * Copyright (C) 2012-2014 Geoff Kuenning
  * Copyright (C) 2012-2014 Sonam Mandal
  * Copyright (C) 2012-2014 Karthikeyani Palanisami
  * Copyright (C) 2012-2014 Philip Shilane
  * Copyright (C) 2012-2014 Sagar Trehan
- * Copyright (C) 2012-2017 Erez Zadok
+ * Copyright (C) 2012-2018 Erez Zadok
  * Copyright (c) 2016-2017 Vinothkumar Raja
  * Copyright (c) 2017-2017 Nidhi Panpalia
- * Copyright (c) 2012-2017 Stony Brook University
- * Copyright (c) 2012-2017 The Research Foundation for SUNY
+ * Copyright (c) 2017-2018 Noopur Maheshwari
+ * Copyright (c) 2018-2018 Rahul Rane
+ * Copyright (c) 2012-2018 Stony Brook University
+ * Copyright (c) 2012-2018 The Research Foundation for SUNY
  * This file is released under the GPL.
  */
 
